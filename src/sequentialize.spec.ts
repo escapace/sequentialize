123ad1670175d78948cbb174f529592512717521
Mark Martirosian <hello@markmartirosian.com>
2024-05-04 06:29:08 +0000

    chore: rewrite tests

--- src/sequentialize.spec.ts
+++ src/sequentialize.spec.ts
@@ -8,47 +8,55 @@ async function delay(string_: string, interval = 10) {
 }
 
 describe('sequentialize', () => {
-  it('execution order', async () =>
-    await new Promise<void>((done) => {
-      const wrap = sequentialize()
+  it('execution order', async () => {
+    const wrap = sequentialize()
 
-      const a = spy()
-      const b = spy()
-      const c = spy()
-      const d = spy()
+    const first = spy()
+    const second = spy()
+    const third = spy()
+    const fourth = spy()
 
-      const one = wrap(delay)
-      const two = wrap(async <T>(value: T) => await Promise.reject(value))
-      const three = wrap(async (value: string) => await Promise.resolve(value))
-      const four = wrap(delay)
+    const soup = wrap(delay)
+    const jelly = wrap(delay)
+    const bean = wrap(
+      async (value: string, interval: number) =>
+        await delay(value, interval).then(async (value) => await Promise.reject(value)),
+    )
+    const peanut = wrap(delay)
 
-      void one('one', 75).then(a)
-      void two('two').catch(b)
-      void three('three').then(c)
-      void one('one-one', 50).then(a)
+    void jelly('first', 200).then(first)
+    void peanut('second', 60).then(second)
+    void bean('third', 30).catch(third)
+    void soup('fourth').then(fourth)
 
-      four('four', 25)
-        .then(d)
-        .then(() => {
-          assert.equal(a.mock.calls.length, 2)
-          assert.equal(b.mock.calls.length, 1)
-          assert.equal(c.mock.calls.length, 1)
-          assert.equal(d.mock.calls.length, 1)
+    void bean('first', 200).catch(first)
+    void peanut('second', 60).then(second)
+    void soup('third', 30).then(third)
+    await jelly('fourth').then(fourth)
 
-          assert.deepEqual(a.mock.calls[0], ['one'])
-          assert.deepEqual(b.mock.calls[0], ['two'])
-          assert.deepEqual(c.mock.calls[0], ['three'])
-          assert.deepEqual(a.mock.calls[1], ['one-one'])
-          assert.deepEqual(d.mock.calls[0], ['four'])
-          //
-          // assert.isTrue(a.firstCall.calledBefore(b.firstCall))
-          // assert.isTrue(b.firstCall.calledBefore(c.firstCall))
-          // assert.isTrue(c.firstCall.calledBefore(a.secondCall))
-          // assert.isTrue(a.secondCall.calledBefore(d.firstCall))
+    assert.equal(first.mock.calls.length, 2)
+    assert.equal(second.mock.calls.length, 2)
+    assert.equal(third.mock.calls.length, 2)
+    assert.equal(fourth.mock.calls.length, 2)
 
-          done()
-        })
-        .catch(console.log)
-      // .then(done)
-    }))
+    assert.deepEqual(first.mock.calls[0], ['first'])
+    assert.deepEqual(second.mock.calls[0], ['second'])
+    assert.deepEqual(third.mock.calls[0], ['third'])
+    assert.deepEqual(fourth.mock.calls[0], ['fourth'])
+
+    assert.equal(first.mock.invocationCallOrder[0], 1)
+    assert.equal(second.mock.invocationCallOrder[0], 2)
+    assert.equal(third.mock.invocationCallOrder[0], 3)
+    assert.equal(fourth.mock.invocationCallOrder[0], 4)
+
+    assert.deepEqual(first.mock.calls[1], ['first'])
+    assert.deepEqual(second.mock.calls[1], ['second'])
+    assert.deepEqual(third.mock.calls[1], ['third'])
+    assert.deepEqual(fourth.mock.calls[1], ['fourth'])
+
+    assert.equal(first.mock.invocationCallOrder[1], 5)
+    assert.equal(second.mock.invocationCallOrder[1], 6)
+    assert.equal(third.mock.invocationCallOrder[1], 7)
+    assert.equal(fourth.mock.invocationCallOrder[1], 8)
+  })
 })
